90af0d39ccc8c8fb07010a707d87659c46d54a2c
Snowball-Wang <wangjieqiang123@163.com>
2018-10-14 16:22:19 +0800

    Solution to problem set 0

---
+++ ps0/ps0.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+#*******************************************************
+#       Filename: ps0.py
+#       Author: Snowball Wang
+#       Mail: wjq1996@mail.ustc.edu.cn
+#       Description: ---
+#       Created on: 2018-10-11 09:24:48
+#*******************************************************
+from numpy import log2
+
+# Ask the user to enter a number "x"
+x = int(input('Enter number x: '))
+# Ask the user to enter a number "y"
+y = int(input('Enter number y: '))
+# Print out number "x", raised to the power "y"
+print("x**y: ", x**y)
+# Print out the log (base 2) of "x"
+print("log(x): ",log2(x))
+
